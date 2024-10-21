897956e385fd24d4c5c6eba07550106e4d38d1b0
ut4 <ut4.l@live.fi>
2024-10-21 11:37:39 +0300

    Make CodeBlockEditForm's save button more visible.

--- frontend/commons-for-edit-app/block-types/code/CodeBlockEditForm.jsx
+++ frontend/commons-for-edit-app/block-types/code/CodeBlockEditForm.jsx
@@ -12,21 +12,34 @@ import {
 } from '../../Form.jsx';
 import {Icon} from '../../Icon.jsx';
 
+const saveBtnEdgeCssStr = '.2rem';
+
 class CodeBlockEditForm extends preact.Component {
     // userCanEditCode;
     // codeInputEl;
+    // saveBtnEl;
     // keyHandlers;
+    // unregisterScrollListner;
     /**
      * @access protected
      */
     componentWillMount() {
         this.userCanEditCode = api.user.getRole() <= api.user.ROLE_ADMIN_EDITOR;
         this.codeInputEl = preact.createRef();
+        this.saveBtnEl = preact.createRef();
         this.keyHandlers = createUndoRedoKeyPressPropagationStopperHandlers();
         const {block} = this.props;
         this.setState(hookForm(this, [
             {name: 'code', value: block.code, validations: [['required'], ['maxLength', validationConstraints.HARD_LONG_TEXT_MAX_LEN]],
-             label: __('Code')},
+             label: __('Code'), onAfterValueChanged: (value, _hasErrors, _source) => {
+                const willUnreveal = this.saveBtnEl.current?.classList.contains('d-invisible') && value !== this.state.committedCode;
+                if (willUnreveal && !this.adjustBntInitiallyTimeout) {
+                    this.adjustBntInitiallyTimeout = setTimeout(() => {
+                        this.adjustSaveChangesBtnEl();
+                        this.adjustBntInitiallyTimeout = null;
+                    }, 160);
+                }
+            }},
         ], {
             committedCode: block.code,
         }));
@@ -49,12 +62,44 @@ class CodeBlockEditForm extends preact.Component {
         setFocusTo(this.codeInputEl);
         const textareaEl = this.codeInputEl.current.inputEl.current;
         window.autosize(textareaEl);
+        //
+        const inspEl = api.inspectorPanel.getOuterEl();
+        const btnEl = this.saveBtnEl.current;
+        this.adjustSaveChangesBtnEl = () => {
+            if (btnEl.classList.contains('d-invisible')) return;
+            const textAreaTop = textareaEl.getBoundingClientRect().top;
+            const inspElTop = inspEl.getBoundingClientRect().top;
+            const diff = textAreaTop - inspElTop;
+            const newIs = inspEl.scrollTop > diff;
+            const prevIs = btnEl.classList.contains('p-fixed');
+
+            if (newIs && !prevIs) {
+                btnEl.classList.add('p-fixed');
+                btnEl.classList.remove('p-absolute');
+                btnEl.style.right = null;
+            } else if (!newIs && prevIs) {
+                btnEl.classList.add('p-absolute');
+                btnEl.classList.remove('p-fixed');
+                btnEl.style.left = null;
+                btnEl.style.right = saveBtnEdgeCssStr;
+                btnEl.style.top = saveBtnEdgeCssStr;
+            }
+
+            if (newIs) {
+                btnEl.style.top = `calc(${Math.max(textAreaTop, inspElTop)}px + .3rem)`;
+                btnEl.style.left = 'calc(var(--menu-column-width-computed) - 2.8rem)';
+            }
+        };
+        const fn = () => this.adjustSaveChangesBtnEl();
+        inspEl.addEventListener('scroll', fn);
+        this.unregisterScrollListner = () => { inspEl.removeEventListener('scroll', fn); };
     }
     /**
      * @access protected
      */
     componentWillUnmount() {
         unhookForm(this);
+        this.unregisterScrollListner();
     }
     /**
      * @param {BlockEditFormProps} props
@@ -79,10 +124,11 @@ class CodeBlockEditForm extends preact.Component {
                         ref={ this.codeInputEl }/>
                     <button
                         onClick={ this.commitCurrentlyTypedCode.bind(this) }
-                        class={ `btn btn-sm p-absolute pt-1${visibilityClsStr}` }
+                        class={ `btn btn-primary btn-sm p-absolute pt-1${visibilityClsStr}` }
                         title={ __('Save changes') }
                         disabled={ saveBtnIsDisabled }
-                        style="right: .2rem;bottom: .2rem;opacity: .8;">
+                        style={ `right: ${saveBtnEdgeCssStr};top: ${saveBtnEdgeCssStr};opacity: .5;` }
+                        ref={ this.saveBtnEl }>
                         <Icon iconId="check" className="size-xs"/>
                     </button>
                 </div>,

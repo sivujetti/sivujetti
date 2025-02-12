a6a96cb41387d03d4beebad30676e8c05f3b5819
ut4 <ut4.l@live.fi>
2025-02-12 09:54:42 +0200

    Change BlockTree::filterBlocks()|traverse() to include blocks from global block trees (uniques) by default. Add BlockTree::filterBlockWithTrees()| traverseWithTrees().

--- backend/sivujetti/src/Block/BlockTree.php
+++ backend/sivujetti/src/Block/BlockTree.php
@@ -4,6 +4,7 @@ namespace Sivujetti\Block;
 
 /**
  * @template BlockCls
+ * @phpstan-type Tree object{id: string, blocks: list<BlockCls>}
  */
 final class BlockTree {
     /**
@@ -23,7 +24,7 @@ final class BlockTree {
     }
     /**
      * @param list<BlockCls> $blocks
-     * @param callable $predicate callable(BlockCls $block): bool
+     * @param callable(BlockCls, Tree, int): bool $predicate
      * @return ?BlockCls
      */
     public static function findBlock(array $branch, callable $predicate): ?object {
@@ -31,48 +32,82 @@ final class BlockTree {
     }
     /**
      * @param list<BlockCls> $blocks
-     * @param callable $predicate callable(BlockCls $block): bool
-     * @param ?object{id: string, blocks: list<BlockCls>} $tree = null
-     * @return array{0: BlockCls|null, 1: object{id: string, blocks: list<BlockCls>}}
+     * @param callable(BlockCls, Tree, int): bool $predicate
+     * @param ?Tree $tree = null
+     * @return array{0: BlockCls|null, 1: Tree}
      */
     public static function findBlockAndTree(array $branch, callable $predicate, ?object $tree = null): array {
-        foreach ($branch as $block) {
-            if (call_user_func($predicate, $block))
-                return [$block, $tree ?? (object) ["id" => "main", "blocks" => $branch]];
-            $sub = $block->type !== "GlobalBlockReference" ? $block->children : $block->__globalBlockTree?->blocks ?? [];
-            if ($sub) {
-                $tree2 = $block->type !== "GlobalBlockReference" ? $tree ?? (object) ["id" => "main", "blocks" => $branch] : $block->__globalBlockTree;
-                $pair = self::findBlockAndTree($sub, $predicate, $tree2);
-                if ($pair[0]) return $pair;
-            }
+        $treeNorm = $tree ?? (object) ["id" => "main", "blocks" => $branch];
+        $closure = $predicate(...);
+        foreach ($branch as $i => $block) {
+            if ($closure($block, $tree, $i))
+                return [$block, $treeNorm];
+            if ($block->type !== "GlobalBlockReference" && ($c = $block->children))
+                $pair = self::findBlockAndTree($c, $closure, $treeNorm);
+            elseif ($block->type === "GlobalBlockReference" && ($gbt = $block->__globalBlockTree ?? null))
+                $pair = self::findBlockAndTree($gbt->blocks, $closure, $gbt);
+            else
+                $pair = [null];
+            if ($pair[0]) return $pair;
         }
-        return [null, $tree ?? (object) ["id" => "main", "blocks" => $branch]];
+        return [null, $treeNorm];
     }
     /**
      * @param list<BlockCls> $blocks
-     * @param callable $predicate callable(BlockCls $block): bool
+     * @param callable(BlockCls, Tree, int): bool $fn
      */
     public static function traverse(array $branch, callable $fn): void {
+        self::traverseWithTrees($branch, $fn);
+    }
+    /**
+     * @param list<BlockCls> $blocks
+     * @param callable(BlockCls, Tree, int): bool $fn
+     * @param ?Tree $tree = null
+     */
+    public static function traverseWithTrees(array $branch, callable $fn, ?object $tree = null): void {
+        $closure = $fn(...);
+        $treeNorm = $tree ?? (object) ["id" => "main", "blocks" => $branch];
         foreach ($branch as $i => $block) {
-            if (call_user_func($fn, $block, $i) === false)
+            if ($closure($block, $treeNorm, $i) === false)
                 return;
-            if ($block->children)
-                self::traverse($block->children, $fn);
+            if ($block->type !== "GlobalBlockReference" && ($c = $block->children))
+                self::traverseWithTrees($c, $closure, $treeNorm);
+            elseif ($block->type === "GlobalBlockReference" && ($gbt = $block->__globalBlockTree ?? null))
+                self::traverseWithTrees($gbt->blocks, $closure, $gbt);
         }
     }
     /**
      * @param list<BlockCls> $blocks
-     * @param callable $predicate callable(BlockCls $block): bool
-     * @param bool $recursive = true
+     * @param callable(BlockCls, Tree, int): bool $predicate
      * @return list<BlockCls>
      */
-    public static function filterBlocks(array $branch, callable $predicate, bool $recursive = true): array {
-        $out = [];
-        foreach ($branch as $block) {
-            if (call_user_func($predicate, $block)) $out[] = $block;
-            if ($recursive && $block->children) {
-                $c = self::filterBlocks($block->children, $predicate, true);
-                if ($c) $out = array_merge($out, $c);
+    public static function filterBlocks(array $branch, callable $predicate): array {
+        return self::filterBlockWithTrees($branch, $predicate)[0];
+    }
+    /**
+     * @param list<BlockCls> $branch
+     * @param callable(BlockCls, Tree, int): bool $predicate
+     * @param ?Tree $tree = null
+     * @return array{0: list<BlockCls>, 1: list<Tree>}
+     */
+    public static function filterBlockWithTrees(array $branch, callable $predicate, ?object $tree = null): array {
+        $out = [[], []];
+        $closure = $predicate(...);
+        $treeNorm = $tree ?? (object) ["id" => "main", "blocks" => $branch];
+        foreach ($branch as $i => $block) {
+            if ($closure($block, $treeNorm, $i)) {
+                $out[0][] = $block;
+                $out[1][] = $treeNorm;
+            }
+            if ($block->type !== "GlobalBlockReference" && ($c = $block->children))
+                $subArr = self::filterBlockWithTrees($c, $closure, $treeNorm);
+            elseif ($block->type === "GlobalBlockReference" && ($gbt = $block->__globalBlockTree))
+                $subArr = self::filterBlockWithTrees($gbt->blocks, $closure, $gbt);
+            else
+                $subArr = [[], []];
+            if ($subArr[0]) {
+                $out[0] = [...$out[0], ...$subArr[0]];
+                $out[1] = [...$out[1], ...$subArr[1]];
             }
         }
         return $out;
